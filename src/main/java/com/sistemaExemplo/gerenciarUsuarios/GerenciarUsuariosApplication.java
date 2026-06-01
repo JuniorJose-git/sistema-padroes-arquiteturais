14ae49effb477cd220e92d47f710a063853f9bd0
JuniorJose-git <joseronaldosantus@gmail.com>
2026-06-01 20:41:31 -0300

    .

---
+++ src/main/java/com/sistemaExemplo/gerenciarUsuarios/GerenciarUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaExemplo.gerenciarUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaExemplo/gerenciarUsuarios/GerenciarUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaExemplo.gerenciarUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
